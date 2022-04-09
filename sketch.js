29b27271bce24b7d725fb137089a719be41137f0
DummyCoder1 <100186733+DummyCoder1@users.noreply.github.com>
2022-04-09 09:03:32 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,16 @@
-var colors = ["white","black", "red", "green", "blue", "yellow", "orange", "violet"];
-var currentColor;
-var y=0
+var colors = ["white","orange","red","green"];
+
 function setup() {
   createCanvas(innerWidth, innerHeight);
   background("white");
-  header();
 }
 
 function draw() {
-  for (var i=0;i<colors.length;i++ ){
-    currColor = new ColorBox(0,y,50,50,colors [i]);
-    currColor.appear();
-    y+=50;
-  } 
+  var y =50;
+  for (i = 0; i < colors.length; i++){
+    currentColor = new Ball (500,y,50,colors [i]) 
+    currentColor. appear();
+    
+     y = y + 50;
+ }
 }
-
-function header() {
-  fill("#404040");
-  rect(0, 0, width, 50);
-
-  textAlign(CENTER);
-  fill("white");
-  textSize(18);
-  text("PAINT", width / 2, 33);
-}
-
